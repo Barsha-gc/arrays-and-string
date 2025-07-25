1b02c5bd969f5fd62d3df5c485667de3ea811a14
Barsha G.C. <barshagc2005@gmail.com>
2025-07-25 12:46:05 +0545

    Remove Capitalize.js from the repository

--- Capitalize.js
+++
@@ -1,8 +0,0 @@
-// Capitalize the first letter of each word in a sentence.
-let sentence = "hello everyone it me park jimin. a musician,dancer,singer and a world famous member of a kpop";
-let capitalized = sentence
-  .split(" ")
-  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-  .join(" ");
-
-console.log(capitalized);
