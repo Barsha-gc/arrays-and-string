971d6ca123c8713e7f8d6b8e6d1f6751e5e31411
Barsha G.C. <barshagc2005@gmail.com>
2025-07-25 12:28:21 +0545

    feat: Count occurrences of a characters in a string

---
+++ count.js
@@ -0,0 +1,11 @@
+// Count occurrences of a characters in a string.
+
+let str = "My name is Barsha Gc. I am from pyuthan municipality 1 khaira";
+let charToCount = "a";
+let count = 0;
+for (let i = 0; i < str.length; i++) {
+  if (str[i] === charToCount) {
+    count++;
+  }
+}
+console.log(`'${charToCount}' appears ${count} times.`);
