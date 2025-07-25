2e827e63698c946c1c920f4a739304513f1b5308
Barsha G.C. <barshagc2005@gmail.com>
2025-07-25 13:02:20 +0545

    feat: Capitalize the first letter of each word in a sentence

---
+++ capitalize.js
@@ -0,0 +1,8 @@
+// . Capitalize the first letter of each word in a sentence.
+let sentence = "hello everyone it me park jimin. a musician,dancer,singer and a world famous member of a kpop";
+let capitalized = sentence
+  .split(" ")
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(" ");
+
+console.log(capitalized);
